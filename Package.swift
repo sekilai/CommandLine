12af1854b7fcb9ecf8c724a35eeddd5738ec69d1
sekirai <sekirai@fsi.co.jp>
2019-05-17 11:51:33 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -4,5 +4,11 @@
 import PackageDescription
 
 let package = Package(
-  name: "CommandLine"
+  name: "CommandLine",
+  targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CommandLine",
+    ]
 )
